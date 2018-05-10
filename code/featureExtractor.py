339eabcc709fd2852e44920878342ac735620ef7
Andrei Chubarau <andrei.chubarau@irystec.com>
2018-05-10 14:10:59 -0400

    removed cached files

--- code/featureExtractor.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-import tools
-
-dataFolderName = "data/"
-trainXFileName = "train_x.csv"
-trainYFileName = "train_y.csv"
-
--- code/readme.txt
+++
@@ -1,1 +0,0 @@
-This is where your code goes. Please branch from origin/master.
